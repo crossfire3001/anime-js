0aadca680244b8622d5d5743c13099244521e28a
crossfire3001 <obiwan2035@mail.ru>
2023-07-29 20:50:06 +0500

    working with arrays and data

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,11 +1,48 @@
 const bgElements = () => {
     const elements = document.querySelectorAll(".set-bg");
 
-    for(let i = 0; i < elements.length; i++) {
+    elements.forEach((element) => {
+        element.style.backgroundImage = `url(${element.dataset.setbg})`;
+    })
+
+
+/*    for(let i = 0; i < elements.length; i++) {
         const src = elements[i].dataset.setbg;
 
         elements[i].style.backgroundImage = `url(${src})`;
-    }
+    } */
+
+    const array = [1, 2, 3, 4, 5];
+
+    const newArray = array.filter((item) =>  item <= 3);
+
+//    console.log(newArray);
+
+    const arrayTwo = [5, 3, 2, 4, 6];
+
+    const newArrayTwo = arrayTwo.sort();
+
+//    console.log(newArrayTwo);
+
+    const arrayThree = [
+        {
+           id: 0, value: 100
+
+        },
+        {
+            id: 2, value: 300
+        },
+        {
+            id: 3, value: 200
+        }
+    ]
+
+    const newArrayThree = arrayThree.sort((a, b) => {
+        return a.value - b.value;
+    });
+
+//    console.log(newArrayThree);
+
 }
 
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,7 +1,44 @@
 const mainData = () => {
+
+    const renderAnimeList = (array, genres) => {
+    console.log(array);
+    console.log(genres);
+    }
+
+    const renderTopAnime = (array) => {
+        const wrapper = document.querySelector('.filter__gallery');
+
+        wrapper.innerHTML = '';
+
+        array.forEach((item) => {
+            wrapper.insertAdjacentHTML('afterbegin', `
+            <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+                <div class="ep">${item.rating}</div>
+                <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+                <h5><a href="/anime-details.html">${item.title}</a></h5>
+            </div>
+            `)
+        })
+
+        wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+            elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+        });
+    }
+
+
+
     fetch('./db.json').then(response => {
         return response.json().then((data) => {
-            console.log(data.anime);
+            const genres = new Set()
+            // сортировка массива в убывающем порядке и сколько нужно элементов показывать
+            renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
+
+            data.anime.forEach((item) => {
+                genres.add(item.ganre)
+            })
+
+            renderAnimeList(data.anime, genres)
+            
         })
     })
 }
